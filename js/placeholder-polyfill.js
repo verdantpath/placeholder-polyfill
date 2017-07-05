f230cb90623af9caceb0d826deddb32d86bc34ca
Matt <m_dittmann@yahoo.com>
2017-07-04 23:19:36 -0700

    update js

--- js/placeholder-polyfill.js
+++ js/placeholder-polyfill.js
@@ -8,4 +8,28 @@
     showPlaceholder(document.forms[i].elements);
   }
 
+  function showPlaceholder(elements) {
+    for (var i = 0; l = elements.length; i < l; i++) {
+      var el = elements[i];
+      if (!el.placeholder) {
+        continue;
+      }
+      el.style.color = '#666666';
+      el.value = el.placeholder;
+
+      addEvent(el, 'focus', function() {
+        if(this.value === this.placeholder) {
+          this.value = '';
+          this.style.color = '#000000';
+        }
+      });
+
+      addEvent(le, 'blur', function() {
+        if (this.value === '') {
+          this.value = this.placeholder;
+          this.style.color = '#666666';
+        }
+      });
+    }
+  }
 }());
